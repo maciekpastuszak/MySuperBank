c283c5b817bfc2a672803489e3723ae4a011a610
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2021-07-27 12:35:57 +0200

    add validation for balance in the account

--- BankAccount.cs
+++ BankAccount.cs
@@ -26,19 +26,36 @@ namespace MySuperBank
         private static int accountNumberSeed = 1234567890;
         private List<Transaction> allTransactions = new List<Transaction>();
 
-        public BankAccount(string name)
+        public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,7 +10,22 @@ namespace MySuperBank
                 Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
 
             account.MakeWithdrawal(120, DateTime.Now, "Hammoc");
-            Console.WriteLine(account.Balance)
+            Console.WriteLine(account.Balance);
+
+            // Test that the initial balances must be positive.
+            BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+                return;
+            }
+
+            account.MakeDeposit(-300, DateTime.Now, "steeling");
         }
     }
 }
